dfc11b75a1bd88b1c630492a8c9c1c52ba31b629
mullity <mullis.tyler.work@gmail.com>
2025-07-09 14:53:32 -0600

    Done with reqs and prettified

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# SDI31-tyler-and-kevin
+# Readme
+
+Created for: SDI31-tyler-and-kevin
 Used for JS Fundamentals III
+
+## How-To-Run:
+
+#### Don't want to carry a pen/ paper? CARRY YOUR WHOLE COMPUTER INSTEAD!
+* Simply navigate to 'index.html'
+* Launch on live server
+* or navigate to localhost:8080 if it's already running
+
+## How-To-Use:
+
+#### Have you ever wanted to write things down but forgot your notpad?
+Look no further! With the To-Do 2 Electric Boogaloo task tracker
+you'll become a productivity machine with features such as...
+
+* Add Tasks!:
+Enter the name of your task in the bottom text box and submit by
+clicking on the "Add Task" button!
+
+* Remove tasks!:
+Finished your task or just don't like it?! Remove it by clicking
+on the "X" button
+
+* Strikethrough!:
+Done with a task but still want to see it? Click on the task
+itself, and you'll keep the text but now with a strike through!
+
+* ~~Bring back your dead goldfish~~
+Experimental! Only avalible version -0.82
+
+
+                                   
--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 console.log('My code is running');
 document.getElementById("submitButton").addEventListener("click", addTask);
 //Spawn exampletask
-addTask("This is an example task. Click the button to the right to delete me.")
+addTask("This is an example task. Click on my text to complete me")
+addTask("I am completed task, delete me using the X button.", true)
 
-function addTask(taskString){
+function addTask(taskString, complete = false) {
     let task = ""
-    if (taskString !== undefined && typeof(taskString)==='string') {
+    if (taskString !== undefined && typeof (taskString) === 'string') {
         task = taskString //use parameter if argument goes in
     } else {
         task = document.getElementById("taskEntry").value; //if addtask is not called programmatically, get the bar instead
     }
-    if (task === ''){
+    if (task === '') {
         alert("You can't just do nothing! Enter a task before submitting") // Edge case check for empty taskEntry bar
         return false
     }
-    
+
     const newTask = document.createElement("li"); // create new element
 
     newTask.innerHTML =
-    `
+        `
     <span class= "task">
-        <span class = "taskText">
-        ${task} 
-        </span>
         <button class="taskDeleteButton">🗙</button>
+        <span class = "taskText">
+        &#8226 ${task} 
+        </span>
+       
     </span>
     `
 
-    // Mark an event as complete
-    newTask.getElementsByClassName("taskText")[0].addEventListener("click",markCompleted)
+    newTask.getElementsByClassName("taskText")[0].addEventListener("click", markCompleted)
+    if (complete === true) {
+        newTask.getElementsByClassName("taskText")[0].className = "completedtask"
+    }
     // Delete a task event
-    newTask.getElementsByClassName("taskDeleteButton")[0].addEventListener("click",removeTask)
+    newTask.getElementsByClassName("taskDeleteButton")[0].addEventListener("click", removeTask)
     //Append task to task container UL
     document.getElementById("taskList").appendChild(newTask); // add our little guy to the task list
     document.getElementById("taskEntry").value = ""; // reset input bar to clean up
@@ -38,13 +42,13 @@ function addTask(taskString){
 
 function markCompleted() {
     //Get calling element 
-    let clickedButton = this 
-    
+    let clickedButton = this
+
     //Set class to "completed task" so CSS can take over, CSS should strike it/fade/etc
-    clickedButton.parentNode.className = "completedtask" 
+    clickedButton.parentNode.className = "completedtask"
     console.log("Completed!");
     // Remove event listener
-    this.removeEventListener("click",markCompleted)
+    this.removeEventListener("click", markCompleted)
 }
 
 function removeTask() {
