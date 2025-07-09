1cafc15e6983279a98088a387da6282b05364061
mullity <mullis.tyler.work@gmail.com>
2025-07-09 15:34:10 -0600

    Crimes against I/O, but persistency is ours

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-console.log('My code is running');
 document.getElementById("submitButton").addEventListener("click", addTask);
+document.getElementById("clearButton").addEventListener("click", clearStorage);
 //Spawn exampletask
-addTask("This is an example task. Click on my text to complete me")
-addTask("I am completed task, delete me using the X button.", true)
+
+startTaskList()
+function startTaskList() {
+    document.getElementById("taskList").innerHTML = localStorage.getItem('taskList');
+    if (document.getElementById("taskList").innerHTML<1){
+        addTask("This is an example task. Click on my text to complete me")
+        addTask("I am completed task, delete me using the X button.", true)
+    }
+    const textList = document.getElementsByClassName("taskText")
+    for (let textBox of textList){
+        textBox.addEventListener("click", markCompleted)
+    }
+    const delButtonList = document.getElementsByClassName("taskDeleteButton")
+    for (let delButton of delButtonList){
+        delButton.addEventListener("click", removeTask)
+    }    
+
+}
 
 function addTask(taskString, complete = false) {
     let task = ""
@@ -20,7 +36,7 @@ function addTask(taskString, complete = false) {
 
     newTask.innerHTML =
         `
-    <span class= "task">
+    <span class= "task" id=${task}>
         <button class="taskDeleteButton">🗙</button>
         <span class = "taskText">
         &#8226 ${task} 
@@ -38,6 +54,9 @@ function addTask(taskString, complete = false) {
     //Append task to task container UL
     document.getElementById("taskList").appendChild(newTask); // add our little guy to the task list
     document.getElementById("taskEntry").value = ""; // reset input bar to clean up
+
+    smashThatSaveButton()
+    console.log(localStorage.getItem(task))
 }
 
 function markCompleted() {
@@ -46,12 +65,23 @@ function markCompleted() {
 
     //Set class to "completed task" so CSS can take over, CSS should strike it/fade/etc
     clickedButton.parentNode.className = "completedtask"
-    console.log("Completed!");
+
     // Remove event listener
     this.removeEventListener("click", markCompleted)
+    smashThatSaveButton();
 }
 
 function removeTask() {
     let clickedButton = this
     clickedButton.parentNode.parentNode.remove()
+    smashThatSaveButton();
+}
+
+function clearStorage(){
+    localStorage.clear()
+    alert("Storage Cleared! Hope it was on purpose :P")
+}
+
+function smashThatSaveButton(){
+    localStorage.setItem('taskList', document.getElementById("taskList").innerHTML);
 }
